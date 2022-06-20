9da3114838ee6da51e1b79e0c7678174c1c7f6c0
apophus <bkilel12@gmail.com>
2022-06-21 00:56:29 +0300

    First non-repeating char challenge

--- codesignals/py/firstNotRepeatingCharacter.py
+++ codesignals/py/firstNotRepeatingCharacter.py
@@ -1,6 +1,19 @@
 from collections import Counter
 
-def solution(s):
 
-    counts = Counter(s)
-    
+def solution(s):
+    tally = Counter(s)
+    res = []
+    res_index = []
+    for char in tally:
+        if tally[char] == 1:
+            res.append(char)
+
+    for i in res:
+        res_index.append(s.find(i))
+    print(tally, res, res_index)
+    print(s[min(res_index)])
+    return s[min(res_index)]
+
+if __name__ == '__main__':
+    solution('abacabad')
