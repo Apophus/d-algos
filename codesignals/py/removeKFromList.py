c9c4fe4da2b7a81536233087107881384ab12f5f
apophus <bkilel12@gmail.com>
2022-07-05 15:51:43 +0300

    added remove k from linkedlist solution - code clean up

---
+++ codesignals/py/removeKFromList.py
@@ -0,0 +1,48 @@
+# Singly-linked lists are already defined with this interface:
+# class ListNode(object):
+#   def __init__(self, x):
+#     self.value = x
+#     self.next = None
+#
+def solution(l, k):
+    c = l
+    while c:
+        if c.next and c.next.value == k:
+            c.next = c.next.next
+        else:
+            c = c.next
+    return l.next if l and l.value == k else l
+
+def solution(l, k):
+    if l == None:
+        return None
+    if l.next == None and l.value == k:
+        return None
+    if l.next == None and l.value != k:
+        return l
+    else:
+        while l.value == k:
+            l = l.next
+            if l == None:
+                return None
+        current = l
+        ahead = current.next
+        while ahead is not None:
+            if ahead.value == k:
+                current.next = ahead.next
+                ahead = current.next
+            else:
+                current = ahead
+                ahead = ahead.next
+        return l
+
+def solution(l, k):
+   m=l
+   while m:
+      if m.next and m.next.value==k:
+         m.next=m.next.next
+      else:
+         m=m.next
+   if l and l.value==k:
+      return l.next
+   return l
