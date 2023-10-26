686676d2d7c824e064541d0d41016f4ada2fcd04
apophus <bkilel12@gmail.com>
2023-10-26 21:54:54 +0300

    Sort linked list. Merge sort recursive solution.

---
+++ leetcode/py/sort_list.py
@@ -0,0 +1,52 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
+
+class Solution:
+    def sortList(self, head):
+        if not head or not head.next:
+             return head
+         
+        left = head
+        right = self.get_mid(head)
+        temp = right.next
+        right.next = None
+        right = temp
+        
+        left = self.sortList(left)
+        right = self.sortList(right)
+        
+        return self.merge(left, right)
+    
+    def get_mid(self, head):
+        slow, fast = head, head.next
+        
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            
+        return slow
+    
+        
+    def merge(self, list1, list2):
+        temp = ListNode()
+        tail = temp
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+                
+            tail = tail.next
+            
+        if list1:
+            tail.next = list1
+        elif list2:
+            tail.next = list2
+            
+        return temp.next
