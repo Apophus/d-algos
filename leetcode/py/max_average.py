051af356dbc734d162dfc1645dd0992d5d0dcd5a
apophus <bkilel12@gmail.com>
2024-10-11 06:58:48 +0300

    MAx avg solution

---
+++ leetcode/py/max_average.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def findMaxAverage(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: float
+        """
+        res = float('-inf')
+        curr_sum = 0
+        for i in range(k):
+            curr_sum += nums[i]
+
+        res = max(res, float(curr_sum / k,3))
+
+        for i in range(k, len(nums)):
+            curr_sum += nums[i]
+            curr_sum -= nums[i - k]
+            res = max(res, curr_sum / k)
+
+        return res
